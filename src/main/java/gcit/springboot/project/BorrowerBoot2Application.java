204201dcce7fb896bc4e26746cb5ad8c734506f3
unknown <samrikonjo700@gmail.com>
2019-11-23 08:06:47 -0800

    first commit

---
+++ src/main/java/gcit/springboot/project/BorrowerBoot2Application.java
@@ -0,0 +1,13 @@
+package gcit.springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowerBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowerBoot2Application.class, args);
+	}
+
+}
